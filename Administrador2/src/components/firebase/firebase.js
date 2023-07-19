43cfb5debaec6153196ce8a2a6e7de87632fd753
CristopherDered <92552306+CristopherDered@users.noreply.github.com>
2023-07-19 01:24:19 -0600

    Update firebase.js

--- Administrador2/src/components/firebase/firebase.js
+++ Administrador2/src/components/firebase/firebase.js
@@ -11,13 +11,7 @@ import { v4 } from 'uuid'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDz2wGAcv7SmDpnz8dhujN9XyPKsSTXCjI",
-    authDomain: "vidanuevasystem-0.firebaseapp.com",
-    projectId: "vidanuevasystem-0",
-    storageBucket: "vidanuevasystem-0.appspot.com",
-    messagingSenderId: "65220964712",
-    appId: "1:65220964712:web:6fc220786123b8053fa6dd",
-    measurementId: "G-4PH288SM2T"
+    // c:
 };
 
 // Initialize Firebase
